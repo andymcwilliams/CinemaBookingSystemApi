726fcacb184cecf22cc1d5d4a9343cff99493a09
andymcwilliams <andrew.mcwilliams65@gmail.com>
2024-02-17 17:08:50 +0000

    Program to choose between in memory and real DB

--- src/CinemaBookingSystemApi/Program.cs
+++ src/CinemaBookingSystemApi/Program.cs
@@ -6,8 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<CinemaBookingSystemContext>(opt =>
+
+var configBuilder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json");
+
+if (builder.Environment.IsDevelopment())
+{
+    configBuilder.AddJsonFile("appsettings.Development.json");
+}
+
+var config = configBuilder.Build();
+
+var useInMemoryDB = config.GetValue<bool>("useInMemoryDB");
+var connectionString = config.GetConnectionString("CinemaBookingSystemConnection");
+
+if(useInMemoryDB){
+    builder.Services.AddDbContext<CinemaBookingSystemContext>(opt =>
     opt.UseInMemoryDatabase("CinemaBookingSystem"));
+} else {
+    builder.Services.AddDbContext<CinemaBookingSystemContext>(opt =>
+    opt.UseSqlServer(connectionString));
+}
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
